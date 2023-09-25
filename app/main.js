8584cf271ebbe1f4f37e12412cabef31a0bb2b94
shivank <shivankkunwar100@gmail.com>
2023-09-25 15:52:23 +0530

    pass 3rd stage

--- app/main.js
+++ app/main.js
@@ -5,8 +5,19 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.write("HTTP/1.1 200 OK\r\n\r\n");
-  socket.end();
+  socket.on("data", (data) => {
+    const request = data.toString();
+    const startLine = request.split("\r\n")[0];
+    const path = startLine.split(" ")[1];
+
+    if (path === "/") {
+      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    } else {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+    }
+  });
+
+ 
   socket.on("close", () => {
     socket.end();
     server.close();
