7b63d7df24b909c990d5a3a53e65ad558d2bd877
shivank <shivankkunwar100@gmail.com>
2023-10-07 13:43:36 +0530

    stage 4 complete

--- app/main.js
+++ app/main.js
@@ -7,24 +7,24 @@ console.log("Logs from your program will appear here!");
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const request = data.toString();
+  
     const startLine = request.split("\r\n")[0];
     const[command, path, reqPath]  = startLine.split(" ");
     console.log(path);
     if (path === "/") {
         socket.write("HTTP/1.1 200 OK\r\n\r\n");
       }
-    if (path.startsWith("/echo/")) {
+    else if (path.startsWith("/echo/")) {
       const randomString = path.substring(6);
       const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${randomString.length}\r\n\r\n${randomString}`;
       socket.write(response);
     } 
-    if(path === "/user-agent"){
-        const [extra, value]= startLine[2].split(" ");
-        console.log(value);
-        console.log(typeof(value));
+    else if(path === "/user-agent"){
+        const [extra, value]= request.split("\r\n")[2].split(" ");
+      
         const response= `${reqPath} 200 OK \r\n\r\nContent-Type: text/plain\r\nContent-Length: ${value.length}\r\n\r\n${value}`;
+        socket.write(response);
     }
-    
     else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
