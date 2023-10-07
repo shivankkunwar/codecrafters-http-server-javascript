fd891a26d9d55ee3753d5297f0fb9b6c7040031d
shivank <shivankkunwar100@gmail.com>
2023-10-07 13:36:16 +0530

    stage 4 complete

--- app/main.js
+++ app/main.js
@@ -8,7 +8,7 @@ const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const request = data.toString();
     const startLine = request.split("\r\n")[0];
-    const path = startLine.split(" ")[1];
+    const[command, path, reqPath]  = startLine.split(" ");
     console.log(path);
     if (path === "/") {
         socket.write("HTTP/1.1 200 OK\r\n\r\n");
@@ -17,7 +17,13 @@ const server = net.createServer((socket) => {
       const randomString = path.substring(6);
       const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${randomString.length}\r\n\r\n${randomString}`;
       socket.write(response);
-    } else {
+    } 
+    if(startline.startsWith("GET /user-agent")){
+        const value= startLine[2].split(" ")[1];
+        const response= `${reqPath} 200 OK \r\n\r\nContent-Type: text/plain\r\nContent-Length: ${value.length}\r\n\r\n${value}`;
+    }
+    
+    else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
     socket.end();
