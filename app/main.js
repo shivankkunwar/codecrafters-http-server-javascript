afb4821082cb65f7620e7e065389e8008c9d8ad2
shivank <shivankkunwar100@gmail.com>
2023-10-13 13:53:49 +0530

    stage 8 completed

--- app/main.js
+++ app/main.js
@@ -1,70 +1,69 @@
 const net = require("net");
 const fs = require('fs');
 const directory = process.argv[2];
-// You can use print statements as follows for debugging, they'll be visible when running tests.
+
 console.log("Logs from your program will appear here!");
 
-// Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const request = data.toString();
-  
     const startLine = request.split("\r\n")[0];
-    const[command, path, reqPath]  = startLine.split(" ");
-    console.log(path);
-    if (path === "/") {
+    const [command, path, reqPath] = startLine.split(" ");
+    
+    if (command === "GET") {
+      if (path === "/") {
         socket.write("HTTP/1.1 200 OK\r\n\r\n");
-      }
-    else if (path.startsWith("/echo/")) {
-      const randomString = path.substring(6);
-      const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${randomString.length}\r\n\r\n${randomString}`;
-      socket.write(response);
-    } 
-    else if(path === "/user-agent"){
+      } else if (path.startsWith("/echo/")) {
+        const randomString = path.substring(6);
+        const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${randomString.length}\r\n\r\n${randomString}`;
+        socket.write(response);
+      } else if(path === "/user-agent"){
         const userAgent = request.split("\r\n").find(line => line.startsWith("User-Agent:")).split(": ")[1];      
         const response= `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`;
         socket.write(response);
-    }
-    else if(path.startsWith("/files/")){
-      const filename = path.substring(7);
-      const filepath = `./${directory}/${filename}`;
-
-      if (command === "POST") {
-        const contentLength = request.split("\r\n").find(line => line.startsWith("Content-Length:")).split(": ")[1];
-        const requestBody = request.split("\r\n\r\n")[1];
-
-        fs.writeFile(filepath, requestBody, (err) => {
-          if (err) {
-            socket.write("HTTP/1.1 500 Internal Server Error\r\n\r\n");
-          } else {
-            socket.write("HTTP/1.1 201 Created\r\n\r\n");
-          }
-          socket.end();
-        });
-      }
-      else if (command === "GET") {
-        if (fs.existsSync(filepath)) {
-          const fileContent = fs.readFileSync(filepath);
-          socket.write(`HTTP/1.1 200 OK\r\n`);
-          socket.write(`Content-Type: application/octet-stream\r\n`);
-          socket.write(`Content-Length: ${fileContent.length}\r\n\r\n`);
-          socket.write(`${fileContent}\r\n`);
+      } else if(path.startsWith("/files/")){
+        let directory = process.argv[3];
+        let file = data.toString().slice(data.toString().indexOf("/") + 7, data.toString().indexOf(" HTTP"))
+        if (fs.existsSync(directory+file)) {
+          let fileContent = fs.readFileSync(directory + file)
+          socket.write(`HTTP/1.1 200 OK\r\n`)
+          socket.write(`Content-Type: application/octet-stream\r\n`)
+          socket.write(`Content-Length: ${fileContent.length}\r\n\r\n`)
+          socket.write(`${fileContent}\r\n`)
+          socket.end()
         } else {
-          socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+          socket.write("HTTP/1.1 404 Not Found\r\n\r\n")
+          socket.end()
+        }
+      } else {
+        socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+      }
+    } else if (command === "POST" && path.startsWith("/files/")) {
+      let filename = path.substring(7);
+      let filepath = directory + filename;
+      let fileData = reqPath.split("\r\n\r\n")[1];
+      
+      fs.writeFile(filepath, fileData, (err) => {
+        if (err) {
+          console.log(err);
+          socket.write("HTTP/1.1 500 Internal Server Error\r\n\r\n");
+        } else {
+          socket.write("HTTP/1.1 201 Created\r\n\r\n");
         }
         socket.end();
-      }
-     }
-    else {
+      });
+    } else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
-
+      socket.end();
     }
-    socket.end();
   });
 
   socket.on("close", () => {
     console.log("socket closed");
-    });
+  });
 });
+
+
+
 //should work for multiple requests
 server.listen(4221, "localhost");
