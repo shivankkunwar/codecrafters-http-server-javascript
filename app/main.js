de6338f29357f08ee181f8fefa6f2c742d1e0245
shivank <shivankkunwar100@gmail.com>
2023-10-13 12:03:11 +0530

    stage 8 completed

--- app/main.js
+++ app/main.js
@@ -8,45 +8,31 @@ console.log("Logs from your program will appear here!");
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const request = data.toString();
-  
-    const startLine = request.split("\r\n")[0];
-    const[command, path, reqPath]  = startLine.split(" ");
-    console.log(path);
-    if (path === "/") {
-        socket.write("HTTP/1.1 200 OK\r\n\r\n");
-      }
-    else if (path.startsWith("/echo/")) {
-      const randomString = path.substring(6);
-      const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${randomString.length}\r\n\r\n${randomString}`;
-      socket.write(response);
-    } 
-    else if(path === "/user-agent"){
-        const userAgent = request.split("\r\n").find(line => line.startsWith("User-Agent:")).split(": ")[1];      
-        const response= `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`;
-        socket.write(response);
-    }
-    else if(path.startsWith("/files/")){
-      let directory = process.argv[3]
-			let file = data.toString().slice(data.toString().indexOf("/") + 7, data.toString().indexOf(" HTTP"))
-			if (fs.existsSync(directory+file)) {
-				let fileContent = fs.readFileSync(directory + file)
-				socket.write(`HTTP/1.1 200 OK\r\n`)
-				socket.write(`Content-Type: application/octet-stream\r\n`)
-				socket.write(`Content-Length: ${fileContent.length}\r\n\r\n`)
-				socket.write(`${fileContent}\r\n`)
-				socket.end()
-			}
-			else {
-				socket.write("HTTP/1.1 404 Not Found\r\n\r\n")
-				socket.end()
-1
-			}
-     }
-    else {
-      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
 
+    const startLine = request.split("\r\n")[0];
+    const [method, path, reqPath] = startLine.split(" ");
+
+    if (method === "POST" && path.startsWith("/files/")) {
+      const filename = path.substring(7);
+      const filepath = `./${directory}/${filename}`;
+
+      socket.on("data", (data) => {
+        requestBody += data.toString();
+      });
+
+      socket.on("end", () => {
+        try {
+          fs.writeFileSync(filepath, requestBody);
+          socket.write("HTTP/1.1 201 Created\r\n\r\n");
+        } catch (err) {
+          socket.write("HTTP/1.1 500 Internal Server Error\r\n\r\n");
+        }
+        socket.end();
+      });
+    } else {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+      socket.end();
     }
-    socket.end();
   });
 
   socket.on("close", () => {
